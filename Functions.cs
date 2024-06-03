130407ea870c5fc678dddf654645c2c40897376c
EchKode <48565771+echkode@users.noreply.github.com>
2024-06-03 17:31:03 +0000

    Add code

---
+++ Functions.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Content.Code.Utility;
+using PhantomBrigade;
+using PhantomBrigade.Functions;
+
+namespace EchKode.PBMods.StandAndShoot.Functions
+{
+	[TypeHinted]
+	[System.Serializable]
+	public sealed class CombatActionValidatePart : ICombatActionValidationFunction
+	{
+		public string socket;
+		public string preset;
+		public Dictionary<string, bool> tags;
+		public bool present;
+
+		public bool IsValid(CombatEntity unitCombat)
+		{
+			if (unitCombat == null || string.IsNullOrEmpty(socket))
+			{
+				return false;
+			}
+
+			var unitPersistent = IDUtility.GetLinkedPersistentEntity(unitCombat);
+			if (unitPersistent == null)
+			{
+				return false;
+			}
+
+			var part = EquipmentUtility.GetPartInUnit(unitPersistent, socket);
+			if (part == null || !part.isPart || part.isWrecked)
+			{
+				return !present;
+			}
+
+			if (!string.IsNullOrEmpty(preset))
+			{
+				var expected = part.hasDataKeyPartPreset && string.CompareOrdinal(part.dataKeyPartPreset.s, preset) == 0;
+				return present == expected;
+			}
+
+			if (tags == null || tags.Count == 0)
+			{
+				return !present;
+			}
+
+			if (part.hasTagCache && part.tagCache.tags != null)
+			{
+				var partTags = part.tagCache.tags;
+				return present == tags.All(kvp => kvp.Value == partTags.Contains(kvp.Key));
+			}
+
+			return present && tags.All(kvp => !kvp.Value);
+		}
+	}
+}
---
+++ ModLink.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using HarmonyLib;
+using PBModManager = PhantomBrigade.Mods.ModManager;
+using UnityEngine;
+
+namespace EchKode.PBMods.StandAndShoot
+{
+	public partial class ModLink : PhantomBrigade.Mods.ModLink
+	{
+		internal static int modIndex;
+		internal static string modID;
+		internal static string modPath;
+
+		public override void OnLoad(Harmony harmonyInstance)
+		{
+			modID = metadata.id;
+			modIndex = PBModManager.metadataPreloadList.FindIndex(mod => StringComparer.Ordinal.Compare(mod.metadata.id, modID) == 0);
+			modPath = metadata.path;
+
+			Debug.LogFormat("Mod {0} ({1}) is executing OnLoad | Path: {2}",
+				modIndex,
+				modID,
+				metadata.path);
+		}
+	}
+}
